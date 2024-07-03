b8dac2c6854940e15f58cd3db97fa2f14d22ad9f
Felipe Siqueira <felipe.fe.augusto13@gmail.com>
2024-07-03 19:24:07 -0300

    desafio

---
+++ app.js
@@ -0,0 +1,19 @@
+let listaGenerica = [];
+
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+
+let listaDeNomes = ['Felipe', 'Nathalia', 'Luiza'];
+console.log(listaDeNomes[0]);
+
+
+let listaDeNomes = ['Felipe', 'Nathalia', 'Luiza'];
+console.log(listaDeNomes[1]);
+
+
+let listaDeNomes = ['Felipe', 'Nathalia', 'Luiza'];
+console.log(listaDeNomes[2]);
